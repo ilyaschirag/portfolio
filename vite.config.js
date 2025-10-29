e4c27f9a5d41ef77ab54c9c8cb92919c1e378b1c
ilyaschirag <142200329+ilyaschirag@users.noreply.github.com>
2025-10-29 22:41:06 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/portfolio/',
   server: {
     host: true,   // 👈 allows access from network (e.g. phone, another PC)
     port: 5173,   // 👈 optional; can change to any available port like 3000
